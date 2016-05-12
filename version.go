7fa5164458f1baded84b243c5e394c61f8ec1f89
Julien Vehent <julien@linuxwall.info>
2016-05-12 08:12:32 -0400

    reorg tools

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
 package main
 
-const version = "20160505.0-74381c9"
-const commit = "74381c9d611b1a3dbf270bbbde604f22542a962a"
+const version = "20160512.0-19fbb91"
+const commit = "19fbb910e2bd81cdd71fba2d1a297852a3ca17e8"
