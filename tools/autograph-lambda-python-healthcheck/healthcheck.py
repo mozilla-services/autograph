8e457121835577c57ab0d95a87761b7aaa542e06
Julien Vehent <julien@linuxwall.info>
2016-08-06 11:12:35 -0400

    Point lambda client to monitoring endpoint

--- tools/autograph-lambda-python-healthcheck/healthcheck.py
+++ tools/autograph-lambda-python-healthcheck/healthcheck.py
@@ -9,50 +9,53 @@ from requests_hawk import HawkAuth
 
 
 def autograph_monitor(event, context):
-    inputdata = "this is a heartbeat message"
-    sigreq = [{
-        "input": base64.b64encode(inputdata),
-    }]
-    r = requests.post("http://ip-172-31-32-24.ec2.internal:8000/sign/data",
-            json=sigreq,
+    inputdata = "AUTOGRAPH MONITORING"
+    r = requests.get("http://localhost:8000/__monitor__",
             auth=HawkAuth(
-                id="alice",
-                key="fs5wgcer9qj819kfptdlp8gm227ewxnzvsuj9ztycsx08hfhzu"
+                id="monitor",
+                key="19zd4w3xirb5syjgdx8atq6g91m03bdsmzjifs2oddivswlu9qs"
             )
         )
     r.raise_for_status()
     sigresp = json.loads(r.text)
 
-    # the public key is converted to regular base64, and loaded
-    pubkeystr = un_urlsafe(sigresp[0]["public_key"])
-    vk = ecdsa.VerifyingKey.from_pem(pubkeystr)
+    for sig in sigresp:
+        # the public key is converted to regular base64, and loaded
+        pubkeystr = un_urlsafe(sig["public_key"])
+        vk = ecdsa.VerifyingKey.from_pem(pubkeystr)
 
-    # the signature is b64 decoded to obtain bytes
-    sigdata = base64.b64decode(un_urlsafe(sigresp[0]["signature"].encode("utf-8")))
+        # the signature is b64 decoded to obtain bytes
+        sigdata = base64.b64decode(un_urlsafe(sig["signature"].encode("utf-8")))
 
-    hashfunc = None
-    if "hash_algorithm" in sigresp[0]:
-        if sigresp[0]["hash_algorithm"] == "sha384":
-            hashfunc = hashlib.sha384
-        if sigresp[0]["hash_algorithm"] == "sha256":
-            hashfunc = hashlib.sha256
-        if sigresp[0]["hash_algorithm"] == "sha512":
-            hashfunc = hashlib.sha512
+        hashfunc = None
+        if "hash_algorithm" in sig:
+            if sig["hash_algorithm"] == "sha384":
+                hashfunc = hashlib.sha384
+            if sig["hash_algorithm"] == "sha256":
+                hashfunc = hashlib.sha256
+            if sig["hash_algorithm"] == "sha512":
+                hashfunc = hashlib.sha512
 
-    # perform verification
-    isgoodsig = False
-    if hashfunc:
-        isgoodsig = vk.verify(sigdata, 
-                inputdata, 
-                hashfunc=hashfunc, 
-                sigdecode=ecdsa.util.sigdecode_string)
-    else:
-        isgoodsig = vk.verify_digest(sigdata,
-                inputdata,
-                sigdecode=ecdsa.util.sigdecode_string)
-    print("signature validation: %s" % isgoodsig)
-    if not isgoodsig:
-        raise
+        # perform verification
+        isgoodsig = False
+        hascontentsig = ""
+        if hashfunc:
+            if "content-signature" in sig:
+                hascontentsig = " (content-signature)"
+                templatedinput = "Content-Signature:\x00" + inputdata
+            else:
+                templatedinput = inputdata
+            isgoodsig = vk.verify(sigdata,
+                    templatedinput,
+                    hashfunc=hashfunc,
+                    sigdecode=ecdsa.util.sigdecode_string)
+        else:
+            isgoodsig = vk.verify_digest(sigdata,
+                    inputdata,
+                    sigdecode=ecdsa.util.sigdecode_string)
+        print("%s%s signature validation: %s" % (sig["signer_id"], hascontentsig, isgoodsig))
+        if not isgoodsig:
+            raise
 
 
 def un_urlsafe(input):
