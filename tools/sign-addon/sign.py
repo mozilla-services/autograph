fb17278d83b55682b7e7dcfdaa30685fb59076a1
Julien Vehent <julien@linuxwall.info>
2017-11-16 08:43:27 -0500

    Rewrite sign-addon tool for python 3

--- tools/sign-addon/sign.py
+++ tools/sign-addon/sign.py
@@ -12,10 +12,6 @@ from requests_hawk import HawkAuth
 from signing_clients.apps import JarExtractor
 
 
-class SigningError(Exception):
-    pass
-
-
 def call_signing(file_path, guid, endpoint, signer, user, key):
     """Get the jar signature and send it to the signing server to be signed."""
 
@@ -24,14 +20,12 @@ def call_signing(file_path, guid, endpoint, signer, user, key):
         temp_filename = temp_file.name
 
     # Extract jar signature.
-    jar = JarExtractor(path=file_path,
-                       outpath=temp_filename,
-                       omit_signature_sections=True,
-                       extra_newlines=True)
+    jar = JarExtractor(path=file_path)
+    signed_manifest = jar.signatures
 
     # create the signing request
     sigreq = [{
-        "input": b64encode(unicode(jar.signatures)),
+        "input": b64encode(bytes(jar.signatures.__str__(), "utf-8")).decode("utf-8"),
         "keyid": signer,
         "options": {
             "id": guid,
@@ -45,15 +39,19 @@ def call_signing(file_path, guid, endpoint, signer, user, key):
 
     # convert the base64 encoded pkcs7 signature back to binary
     if response.status_code != 201:
-        msg = u'Posting to add-on signing failed: {0}'.format(response.text)
-        raise SigningError(msg)
+        print('Posting to add-on signing failed: {0}'.format(response.text))
+        exit(1)
 
     sigresp = json.loads(response.text)
     pkcs7 = b64decode(sigresp[0]["signature"])
-    jar.make_signed(pkcs7, sigpath=u'mozilla')
+    jar.make_signed(
+        signed_manifest=signed_manifest.__str__(),
+        signature=pkcs7,
+        sigpath=u'mozilla',
+        outpath=temp_filename)
     shutil.move(temp_filename, file_path)
 
-    print "{0} signed!".format(file_path)
+    print("{0} signed!".format(file_path))
 
 
 def get_guid(file_path):
@@ -63,23 +61,24 @@ def get_guid(file_path):
     if 'install.rdf' in zlist:
         try:
             with z.open('install.rdf') as install:
-                match = re.search(r'[\w\.-]+@[\w\.-]+', install.read())
+                match = re.search(r'[\w\.-]+@[\w\.-]+', install.read().decode("utf-8"))
                 return match.group(0)
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print("Failed to get guid from install.rdf:", sys.exc_info()[0])
             exit(1)
     elif 'manifest.json' in zlist:
         try:
             with z.open('manifest.json') as manifest:
-                m = json.loads(manifest.read())
+                m = json.load(manifest)
                 return m['applications']['gecko']['id']
         except:
-            print "Unexpected error:", sys.exc_info()[0]
+            print("Failed to get guid from manifest.json:", sys.exc_info()[0])
             exit(1)
     # we didn't find the guid, bail
     else:
-        print "Could not find guid, check add-on"
+        print("Could not find guid, check add-on")
         exit(1)
+    print("bar")
 
 
 def main():
