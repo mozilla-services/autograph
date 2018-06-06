c9cf43052d827012e92d41de877a41c3df9c85b4
Julien Vehent <julien@linuxwall.info>
2018-06-06 11:50:21 -0400

    mar: use go.mozilla.org/mar

--- signer/mar/mar.go
+++ signer/mar/mar.go
@@ -2,9 +2,11 @@ package mar
 
 import (
 	"crypto"
+	"crypto/rsa"
 
 	"github.com/pkg/errors"
 	"go.mozilla.org/autograph/signer"
+	"go.mozilla.org/mar"
 )
 
 const (
@@ -17,7 +19,7 @@ type MARSigner struct {
 }
 
 // New initializes a mar signer using a configuration
-func New(conf signer.Configuration) (s *APKSigner, err error) {
+func New(conf signer.Configuration) (s *MARSigner, err error) {
 	s = new(MARSigner)
 	if conf.Type != Type {
 		return nil, errors.Errorf("mar: invalid type %q, must be %q", conf.Type, Type)
@@ -47,31 +49,33 @@ func (s *MARSigner) Config() signer.Configuration {
 	}
 }
 
-func (s *APKSigner) SignFile(input []byte, options interface{}) (signer.SignedFile, error) {
-	// make MAR hash
-	signableBlock, err := makeMARSignableBlock(input)
+func (s *MARSigner) SignFile(input []byte, options interface{}) (signer.SignedFile, error) {
+	var marFile mar.File
+	err := mar.Unmarshal(input, &marFile)
 	if err != nil {
-		return nil, errors.Wrap(err, "mar: failed to make MAR signable block")
+		return nil, errors.Wrap(err, "mar: failed to unmarshal input file")
 	}
-	// sign hash
 
-	// pack signature in mar
-	err := packSignatureInMar
-	return
+	// flush the signatures if any is present, we'll make new ones
+	marFile.SignaturesHeader.NumSignatures = uint32(0)
+	marFile.Signatures = nil
+
+	marFile.PrepareSignature(s.signingKey.(*rsa.PrivateKey))
+	err = marFile.FinalizeSignatures()
+	if err != nil {
+		return nil, errors.Wrap(err, "mar: failed to finalize signature")
+	}
+
+	// write out the MAR file
+	output, err := marFile.Marshal()
+	if err != nil {
+		return nil, errors.Wrap(err, "mar: failed to marshal signed file")
+	}
+	return output, nil
 }
 
-func (s *APKSigner) SignData(sigfile []byte, options interface{}) (signer.Signature, error) {
-	return
-}
-
-func (sig *Signature) Verify() error {
-	return
-}
-
-// Signature is a RSA PKCS1 signature
-type Signature struct {
-	Data     []byte
-	Finished bool
+func (s *MARSigner) SignData(sigfile []byte, options interface{}) (signer.Signature, error) {
+	return nil, nil
 }
 
 // Options is empty for this signer type
