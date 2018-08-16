a6147aa0c46a2f362a9de1da5ee048b284f7bbdc
Greg Guthe <gguthe@mozilla.com>
2018-08-16 15:52:32 -0400

    remove vendored autograph

--- vendor/go.mozilla.org/autograph/version.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-cd "$(dirname "$0")"
-printf '{"commit":"%s","version":"%s","source":"https://go.mozilla.org/autograph","build":"https://travis-ci.org/mozilla-services/autograph"}\n' \
-    "$(git rev-parse HEAD)" \
-    "$(git describe --abbrev=0)" > version.json
