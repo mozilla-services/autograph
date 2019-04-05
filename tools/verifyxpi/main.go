6dd866882b1fe9c8ff122f63f2557e342d3d40c1
Julien Vehent <julien@linuxwall.info>
2019-04-05 10:15:22 -0400

    add verifyxpi tool

---
+++ tools/verifyxpi/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"crypto/x509"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"go.mozilla.org/autograph/signer"
+	"go.mozilla.org/autograph/signer/xpi"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("usage: verifyxpi <xpi_file> <root_pem>")
+	}
+	data, err := ioutil.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	roots := x509.NewCertPool()
+	rootContent, err := ioutil.ReadFile(os.Args[2])
+	if err != nil {
+		log.Fatalf("failed to read roots from path %s: %s", os.Args[2], err)
+	}
+	ok := roots.AppendCertsFromPEM(rootContent)
+	if !ok {
+		log.Fatalf("failed to add root certs to pool")
+	}
+	err = xpi.VerifySignedFile(signer.SignedFile(data), roots, xpi.Options{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("signature ok")
+}
