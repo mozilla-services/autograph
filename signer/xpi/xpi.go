a36f73c059c01662c52c42d7ca847807f8f34450
cunyx <38323421+cunyx@users.noreply.github.com>
2019-04-14 12:00:17 +0000

    xpi owner - part 17 - Adapt error messages in "verifyPKCS7SignatureRoundTrip"
    Use the variables "pkcs7SignatureOwnerPath" and "pkcs7SigOwnerPath" instead of
    the hard coded file paths defaults.

--- signer/xpi/xpi.go
+++ signer/xpi/xpi.go
@@ -524,12 +524,12 @@ func verifyPKCS7SignatureRoundTrip(signedFile signer.SignedFile, truststore *x50
 	pkcs7SignatureOwnerPath, pkcs7SigOwnerPath := GetOwnerPaths(owner)
 	sigStrBytes, err := readFileFromZIP(signedFile, pkcs7SigOwnerPath)
 	if err != nil {
-		return errors.Wrapf(err, "failed to read PKCS7 signature META-INF/mozilla.rsa")
+		return errors.Errorf("failed to read PKCS7 signature %q : %v", pkcs7SigOwnerPath, err)
 	}
 	sigStr := base64.StdEncoding.EncodeToString(sigStrBytes)
 	sigData, err := readFileFromZIP(signedFile, pkcs7SignatureOwnerPath)
 	if err != nil {
-		return errors.Wrapf(err, "failed to read META-INF/mozilla.sf")
+		return errors.Errorf("failed to read %q : %v", pkcs7SignatureOwnerPath, err)
 	}
 
 	// convert string format back to signature
