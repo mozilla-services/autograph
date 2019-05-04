441122d40312b0436821d8acc60e8019729a67e9
Julien Vehent [:ulfr] <jvehent@users.noreply.github.com>
2019-05-04 05:28:41 -0400

    Update x509.go

--- signer/xpi/x509.go
+++ signer/xpi/x509.go
@@ -112,7 +112,6 @@ func (s *XPISigner) makeTemplate(cn string) *x509.Certificate {
 		// set the start date of the EE cert before the expiration of the AMO intermediate
 		// from armagaddon. https://bugzilla.mozilla.org/show_bug.cgi?id=1548973
 		NotBefore:          time.Unix(1556300000, 0),
-
 		NotAfter:           time.Now().Add(8760 * time.Hour), // one year
 		SignatureAlgorithm: s.issuerCert.SignatureAlgorithm,
 		KeyUsage:           x509.KeyUsageDigitalSignature,
