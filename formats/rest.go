0f18f261fbaa8b89ad7a900d8228cb9a72e7c999
Julien Vehent <julien@linuxwall.info>
2019-08-09 16:37:37 -0400

    move api request structs to formats package

---
+++ formats/rest.go
@@ -0,0 +1,23 @@
+package formats
+
+// SignatureRequest is sent by an autograph client to request
+// a signature on input data
+type SignatureRequest struct {
+	Input   string `json:"input"`
+	KeyID   string `json:"keyid,omitempty"`
+	Options interface{}
+}
+
+// SignatureResponse is returned by autograph to a client with
+// a signature computed on input data
+type SignatureResponse struct {
+	Ref        string      `json:"ref"`
+	Type       string      `json:"type"`
+	Mode       string      `json:"mode"`
+	SignerID   string      `json:"signer_id"`
+	PublicKey  string      `json:"public_key"`
+	Signature  string      `json:"signature,omitempty"`
+	SignedFile string      `json:"signed_file,omitempty"`
+	X5U        string      `json:"x5u,omitempty"`
+	SignerOpts interface{} `json:"signer_opts,omitempty"`
+}
