30a09dd5678a101afb044ed867c97262c497dc76
Greg Guthe <gguthe@mozilla.com>
2019-11-15 13:45:49 -0500

    add authbackend iface with in-memory impl

--- memory_backend.go
+++ memory_backend.go
@@ -1,1 +1,16 @@
 package main
+
+// authBackend is an interface for finding HAWK users and their
+// permissions
+type authBackend interface {
+}
+
+// inMemoryBackend is an authBackend that loads a config and stores
+// that auth info in memory
+type inMemoryBackend struct {
+}
+
+// newInMemoryAuthBackend returns an empty inMemoryBackend
+func newInMemoryAuthBackend() (backend *inMemoryBackend) {
+	return &inMemoryBackend{}
+}
