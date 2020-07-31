9d48c791a9bc0b3ab1417e6cf126d356a183aa75
Gregory Terzian <gterzian@users.noreply.github.com>
2020-07-31 16:32:42 +0800

    monitor: add dedicated monitor struct/handler

--- monitor_handler.go
+++ monitor_handler.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"sync"
 	"time"
 
 	log "github.com/sirupsen/logrus"
-
-	"go.mozilla.org/autograph/formats"
-	"go.mozilla.org/autograph/signer"
 )
 
 const monitorAuthID = "monitor"
@@ -19,10 +13,10 @@ const monitorAuthID = "monitor"
 // MonitoringInputData is the data signed by the monitoring handler
 var MonitoringInputData = []byte(`AUTOGRAPH MONITORING`)
 
-func (a *autographer) handleMonitor(w http.ResponseWriter, r *http.Request) {
+func (m *monitor) handleMonitor(w http.ResponseWriter, r *http.Request) {
 	rid := getRequestID(r)
 	starttime := time.Now()
-	userid, err := a.authorize(r, []byte(""))
+	userid, err := m.authorize(r, []byte(""))
 	if err != nil {
 		httpError(w, r, http.StatusUnauthorized, "authorization verification failed: %v", err)
 		return
@@ -32,92 +26,28 @@ func (a *autographer) handleMonitor(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	signers := a.getSigners()
-	sigerrstrs := make([]string, len(signers))
-	sigresps := make([]formats.SignatureResponse, len(signers))
-	var wg sync.WaitGroup
-	for i, s := range signers {
-		wg.Add(1)
+	// Wait until the results have been populated with an initial check
+	<-m.initialized
 
-		go func(i int, s signer.Signer) {
-			defer wg.Done()
+	m.RLock()
 
-			// First try the DataSigner interface. If the signer doesn't
-			// implement it, try the FileSigner interface. If that's still
-			// not implemented, return an error.
-			if _, ok := s.(signer.DataSigner); ok {
-				// sign with data set to the base64 of the string 'AUTOGRAPH MONITORING'
-				sig, err := s.(signer.DataSigner).SignData(MonitoringInputData, s.(signer.DataSigner).GetDefaultOptions())
-				if err != nil {
-					sigerrstrs[i] = fmt.Sprintf("signing failed with error: %v", err)
-					return
-				}
-
-				encodedsig, err := sig.Marshal()
-				if err != nil {
-					sigerrstrs[i] = fmt.Sprintf("encoding failed with error: %v", err)
-					return
-				}
-				sigerrstrs[i] = ""
-				sigresps[i] = formats.SignatureResponse{
-					Ref:        id(),
-					Type:       s.Config().Type,
-					Mode:       s.Config().Mode,
-					SignerID:   s.Config().ID,
-					PublicKey:  s.Config().PublicKey,
-					Signature:  encodedsig,
-					X5U:        s.Config().X5U,
-					SignerOpts: s.Config().SignerOpts,
-				}
-				return
-			}
-
-			if _, ok := s.(signer.FileSigner); ok {
-				// Signers that only implement the FileSigner interface must
-				// also implement the TestFileGetter interface to return a valid
-				// test file that can be used here to monitor the signer.
-				if _, ok := s.(signer.TestFileGetter); !ok {
-					sigerrstrs[i] = fmt.Sprintf("signer %q implements FileSigner but not the TestFileGetter interface", s.Config().ID)
-					return
-				}
-				output, err := s.(signer.FileSigner).SignFile(s.(signer.TestFileGetter).GetTestFile(), s.(signer.FileSigner).GetDefaultOptions())
-				if err != nil {
-					sigerrstrs[i] = fmt.Sprintf("signing failed with error: %v", err)
-					return
-				}
-				signedfile := base64.StdEncoding.EncodeToString(output)
-				sigerrstrs[i] = ""
-				sigresps[i] = formats.SignatureResponse{
-					Ref:        id(),
-					Type:       s.Config().Type,
-					Mode:       s.Config().Mode,
-					SignerID:   s.Config().ID,
-					PublicKey:  s.Config().PublicKey,
-					SignedFile: signedfile,
-					X5U:        s.Config().X5U,
-					SignerOpts: s.Config().SignerOpts,
-				}
-				return
-			}
-
-			sigerrstrs[i] = fmt.Sprintf("signer %q does not implement DataSigner or FileSigner interfaces", s.Config().ID)
-			return
-		}(i, s)
-	}
-	wg.Wait()
-	for _, errstr := range sigerrstrs {
+	for _, errstr := range m.sigerrstrs {
 		if errstr != "" {
+			m.RUnlock()
 			httpError(w, r, http.StatusInternalServerError, errstr)
 			return
 		}
 	}
 
-	respdata, err := json.Marshal(sigresps)
+	respdata, err := json.Marshal(m.sigresps)
+
+	m.RUnlock()
+
 	if err != nil {
 		httpError(w, r, http.StatusInternalServerError, "signing failed with error: %v", err)
 		return
 	}
-	if a.debug {
+	if m.debug {
 		log.Printf("signature response: %s", respdata)
 	}
 	w.Header().Add("Content-Type", "application/json")
