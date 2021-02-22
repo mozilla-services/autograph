80194c8d5c5bb7a0ef82cbb25417e44d47a2c028
Greg Guthe <gguthe@mozilla.com>
2021-02-22 09:23:23 -0500

    tools: integration test addon signing against local test fixture

--- tools/autograph-client/integration_test_xpis.sh
+++ tools/autograph-client/integration_test_xpis.sh
@@ -2,23 +2,19 @@
 
 set -e
 
-# CI script to fetch a test XPI and check default webextensions
-# signers can sign it with various signature options
-# autograph and its go client can sign APKs
+# CI script to check default webextensions signers sign a test webext
+# fixture with various signature options autograph
 
-XPI_URL=${XPI_URL:-"https://addons.mozilla.org/firefox/downloads/file/935711/pomodoro_clock-1.1.1-an+fx-windows.xpi"}
 AUTOGRAPH_URL=${AUTOGRAPH_URL:?}
 SIGNER_ID_PREFIX=${SIGNER_ID_PREFIX:-""}
 
-# TODO: local file support e.g. recognize and don't try to fetch file:// urls
-wget -t 5 $XPI_URL
 
 SIGNER_ID=${SIGNER_ID_PREFIX}webextensions-rsa \
 	 TRUST_ROOTS=dev-webext-rsa-root.pem \
 	 TARGET="$AUTOGRAPH_URL" \
-         ./build_test_xpis.sh pomodoro_clock-1.1.1-an+fx-windows.xpi
+         ./build_test_xpis.sh /app/src/autograph/signer/xpi/test/fixtures/ublock_origin-1.33.2-an+fx.xpi
 
 SIGNER_ID=${SIGNER_ID_PREFIX}extensions-ecdsa \
 	 TRUST_ROOTS=dev-ext-ecdsa-root.pem \
 	 TARGET="$AUTOGRAPH_URL" \
-	 ./build_test_xpis.sh pomodoro_clock-1.1.1-an+fx-windows.xpi
+	 ./build_test_xpis.sh /app/src/autograph/signer/xpi/test/fixtures/ublock_origin-1.33.2-an+fx.xpi
