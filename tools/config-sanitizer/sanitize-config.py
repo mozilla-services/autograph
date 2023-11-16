682164c5d5ac169f430f9bb7331a2f420c6c0c4b
Hal Wine <hwine@mozilla.com>
2023-11-16 08:36:45 -0800

    Add more info
    Split authorizations into 2 types: app & edge. Even though they have the
    same name in the yaml file, and share a field name (`id`), they are very
    different.
    
    Add more information for the certificate. In particular, output sha1
    fingerprints, which is what Google Play provides to identify signing
    keys.

--- tools/config-sanitizer/sanitize-config.py
+++ tools/config-sanitizer/sanitize-config.py
@@ -5,8 +5,16 @@ import hashlib
 import sys
 from typing import List
 
+from cryptography import x509
+from cryptography.hazmat.primitives import hashes
 import pydantic
 import yaml
+
+class CertInfo(pydantic.BaseModel):
+    pem: str | None
+    pem_hash: str | None
+    fp_sha1: str | None
+    fp_sha256: str | None
 
 class Signer(pydantic.BaseModel):
     id_: str
@@ -19,13 +27,21 @@ class Signer(pydantic.BaseModel):
     issuerprivatekey: str | None
     issuercert: str | None
     cacert: str | None
+    fp_sha1: str | None
+    fp_sha256: str | None
 
-class Authorization(pydantic.BaseModel):
-    id_: str | None
+class AuthorizationBase(pydantic.BaseModel):
     key: str
-    client_token: str | None
-    signers: List[str] | None
-    signer: str | None
+
+
+class AuthorizationEdge(AuthorizationBase):
+    client_token: str
+    signer: str
+
+
+class AuthorizationApp(AuthorizationBase):
+    id_: str
+    signers: List[str]
 
 
 def sanitize(secret: str) -> str | None:
@@ -34,16 +50,37 @@ def sanitize(secret: str) -> str | None:
         return None
     return hashlib.sha256(secret.encode()).hexdigest()
 
-def gather_authorizations(doc:dict) -> List[Authorization]:
+def gather_authorizations_app(doc:dict) -> List[AuthorizationApp]:
     authorizations = []
     if "authorizations" in doc:
         for data in doc["authorizations"]:
+            if "client_token" in data:
+                assert "id" not in data
+                continue
             try:
-                auth = Authorization(
+                auth = AuthorizationApp(
                         id_=data.get("id"),
                         key=sanitize(data.get("key")),
-                        client_token=sanitize(data.get("client_token")),
                         signers=data.get("signers"),
+                        )
+                authorizations.append(auth)
+            except Exception as e:
+                print(f"choked on {data}")
+                raise
+    return authorizations
+
+
+def gather_authorizations_edge(doc:dict) -> List[AuthorizationEdge]:
+    authorizations = []
+    if "authorizations" in doc:
+        for data in doc["authorizations"]:
+            if "id" in data:
+                assert "client_token" not in data
+                continue
+            try:
+                auth = AuthorizationEdge(
+                        client_token=data.get("client_token"),
+                        key=sanitize(data.get("key")),
                         signer=data.get("signer"),
                         )
                 authorizations.append(auth)
@@ -53,20 +90,41 @@ def gather_authorizations(doc:dict) -> List[Authorization]:
     return authorizations
 
 
+def extract_cert_info(data: str | None) -> CertInfo:
+    if not data:
+        cert_info = CertInfo(pem=None, pem_hash=None, fp_sha1=None,
+                             fp_sha256=None)
+    else:
+        cert = x509.load_pem_x509_certificate(data.encode())
+        cert_info = CertInfo(
+                pem=data,
+                pem_hash=sanitize(data),
+                fp_sha1=cert.fingerprint(hashes.SHA1()).hex(sep=":",
+                                                            bytes_per_sep=1),
+                fp_sha256=cert.fingerprint(hashes.SHA256()).hex(sep=":",
+                                                                bytes_per_sep=1),
+                )
+    return cert_info
+
+
+
 def gather_signers(doc:dict) -> List[Signer]:
     signers = []
     if "signers" in doc:
         for data in doc["signers"]:
             try:
+                cert_info = extract_cert_info(data.get("certificate"))
                 signer = Signer(
                         id_=data["id"],
                         type_=data["type"],
                         mode=data.get("mode"),
                         private_key=sanitize(data.get("privatekey")),
-                        certificate=sanitize(data.get("certificate")),
+                        certificate=cert_info.pem_hash,
                         issuerprivatekey=data.get("issuerprivatekey"),
                         issuercert=sanitize(data.get("issuercert")),
                         cacert=sanitize(data.get("cacert")),
+                        fp_sha1=cert_info.fp_sha1,
+                        fp_sha256=cert_info.fp_sha256,
                         )
                 signers.append(signer)
             except Exception as e:
@@ -85,10 +143,16 @@ def output_signers(signers: List[Signer]) -> None:
             writer.writerow(s.model_dump())
 
 
-def output_authorizations(authorizations: List[Authorization]) -> None:
+def output_authorizations(authorizations: List[AuthorizationBase]) -> None:
     if not len(authorizations):
         return
-    with open("authorizations.csv", "w", newline="") as csvfile:
+    if isinstance(authorizations[0], AuthorizationApp):
+        domain = "app"
+    elif isinstance(authorizations[0], AuthorizationEdge):
+        domain = "edge"
+    else:
+        raise TypeError(f"Unexpected type: {type(authorizations[0])}")
+    with open(f"authorizations_{domain}.csv", "w", newline="") as csvfile:
         writer = csv.DictWriter(csvfile,
                                 fieldnames=authorizations[0].model_dump().keys())
         writer.writeheader()
@@ -112,16 +176,20 @@ def main() -> int:
     """
     
     signers = []
-    authorizations = []
+    authorizations_app = []
+    authorizations_edge = []
     for doc in yaml.safe_load_all(sys.stdin):
         assert isinstance(doc, dict)
         signers.extend(gather_signers(doc))
-        authorizations.extend(gather_authorizations(doc))
+        authorizations_app.extend(gather_authorizations_app(doc))
+        authorizations_edge.extend(gather_authorizations_edge(doc))
 
     print(f"found {len(signers)} signers")
-    print(f"found {len(authorizations)} authorizations")
+    print(f"found {len(authorizations_app)} app authorizations")
+    print(f"found {len(authorizations_edge)} edge authorizations")
     output_signers(signers)
-    output_authorizations(authorizations)
+    output_authorizations(authorizations_app)
+    output_authorizations(authorizations_edge)
     
     return 0
 
