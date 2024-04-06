1dfebc364739e354df676df4a508b711683ba43b
Jeff Hodges <jeff@somethingsimilar.com>
2024-04-06 01:01:16 -0700

    quick key agility for AWS RDS root

--- database/connect.go
+++ database/connect.go
@@ -33,12 +33,17 @@ type Transaction struct {
 
 // Config holds the parameters to connect to a database
 type Config struct {
-	Name                string
-	User                string
-	Password            string
-	Host                string
-	SSLMode             string
-	SSLRootCert         string
+	Name        string
+	User        string
+	Password    string
+	Host        string
+	SSLMode     string
+	SSLRootCert string
+	// SSLRootCertificates is a list of root certificates to attempt to verify
+	// the TLS connection to the PostgreSQL database with. These will be tried
+	// in the order they are listed. This is useful when the database server
+	// needs to rotate its keys.
+	SSLRootCertificates []string `yaml:"sslrootcertificates"` // Matching the no-underscore style elsewhere
 	MaxOpenConns        int
 	MaxIdleConns        int
 	MonitorPollInterval time.Duration
@@ -46,18 +51,55 @@ type Config struct {
 
 // Connect creates a database connection and returns a handler
 func Connect(config Config) (*Handler, error) {
-	var dsn string
+	dsns := constructDSNStrings(config)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	var outerErr error
+	for _, dsn := range dsns {
+		h, err := connect(ctx, config, dsn)
+		if err == nil {
+			return h, nil
+		}
+		outerErr = err
+	}
+	return nil, outerErr
+}
+
+// dsnString is a PostgreSQL database connection string. It's mostly to prevent
+// the internal API here being too stringly-typed.
+type dsnString string
+
+// construcDSNStrings returns a list of DSN strings to try connecting to the
+// PostgreSQL database with. If an AUTOGRAPH_DB_DSN env var is set, the list
+// will contain only it. Otherwise, it will create a DSN for each certificate
+// that autograph is configured to try when verifiying the TLS connection. This
+// is to allow us to switch to a new AWS RDS certificate without extended
+// downtime.
+func constructDSNStrings(config Config) []dsnString {
 	if os.Getenv("AUTOGRAPH_DB_DSN") != "" {
-		dsn = os.Getenv("AUTOGRAPH_DB_DSN")
-	} else {
+		return []dsnString{dsnString(os.Getenv("AUTOGRAPH_DB_DSN"))}
+	}
+	var rootCerts []string
+	if config.SSLRootCert != "" {
+		rootCerts = append(rootCerts, config.SSLRootCert)
+	}
+	rootCerts = append(rootCerts, config.SSLRootCertificates...)
+	var dsns []dsnString
+	for _, rootCert := range rootCerts {
 		userPass := url.UserPassword(config.User, config.Password)
 		if config.SSLMode == "" {
 			config.SSLMode = "disable"
 		}
-		dsn = fmt.Sprintf("postgres://%s@%s/%s?sslmode=%s&sslrootcert=%s",
-			userPass.String(), config.Host, config.Name, config.SSLMode, config.SSLRootCert)
+		dsn := fmt.Sprintf("postgres://%s@%s/%s?sslmode=%s&sslrootcert=%s",
+			userPass.String(), config.Host, config.Name, config.SSLMode, rootCert)
+		dsns = append(dsns, dsnString(dsn))
 	}
-	dbfd, err := sql.Open("postgres", dsn)
+	return dsns
+}
+
+func connect(ctx context.Context, config Config, dsn dsnString) (*Handler, error) {
+	dbfd, err := sql.Open("postgres", string(dsn))
 	if err != nil {
 		return nil, fmt.Errorf("failed to open database connection: %w", err)
 	}
@@ -68,7 +110,7 @@ func Connect(config Config) (*Handler, error) {
 		dbfd.SetMaxIdleConns(config.MaxIdleConns)
 	}
 	h := &Handler{dbfd}
-	dbCheckCtx, dbCancel := context.WithTimeout(context.Background(), 5*time.Second)
+	dbCheckCtx, dbCancel := context.WithTimeout(ctx, 5*time.Second)
 	defer dbCancel()
 	err = h.CheckConnectionContext(dbCheckCtx)
 	return h, err
