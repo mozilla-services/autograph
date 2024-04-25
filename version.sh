8f7dcc3856ad704533e758ed8c42f0db6ea435ff
Naomi Kirby <oskirby@gmail.com>
2024-04-25 16:34:19 -0700

    Generate a sensible version.json even when developing locally

--- version.sh
+++ version.sh
@@ -3,10 +3,24 @@
 set -e
 
 cd "$(dirname "$0")"
+
 # create a version.json per https://github.com/mozilla-services/Dockerflow/blob/main/docs/version_object.md
-printf '{"commit":"%s","version":"%s","source":"https://github.com/%s/%s","build":"%s"}\n' \
-"$CIRCLE_SHA1" \
-"$CIRCLE_TAG" \
-"$CIRCLE_PROJECT_USERNAME" \
-"$CIRCLE_PROJECT_REPONAME" \
-"$CIRCLE_BUILD_URL" > version.json
+if [ -n "$CIRCLE_SHA1" ]; then
+    # We are running in CircleCI.
+    printf '{"commit":"%s","version":"%s","source":"https://github.com/%s/%s","build":"%s"}\n' \
+        "$CIRCLE_SHA1" \
+        "$CIRCLE_TAG" \
+        "$CIRCLE_PROJECT_USERNAME" \
+        "$CIRCLE_PROJECT_REPONAME" \
+        "$CIRCLE_BUILD_URL" > version.json
+elif [ -d .git ]; then
+    # Try pulling info from git directly.
+    printf '{"commit":"%s","version":"%s","source":"%s","build":"%s"}\n' \
+        "$(git rev-parse HEAD)" \
+        "$(git describe --tags)" \
+        "$(git remote get-url origin)" \
+        "unknown" > version.json
+elif [ ! -f version.json ]; then
+    # Otherwise, give up and create an empty version file.
+    echo '{"commit":"","version":"","source":"","build":""}\n' > version.json
+fi
