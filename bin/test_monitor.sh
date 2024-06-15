11e40bc59dae9d4a0b10dbe697d20558679afe0e
Naomi Kirby <oskirby@gmail.com>
2024-06-15 09:33:28 -0700

    Fix monitor result checking in test

--- bin/test_monitor.sh
+++ bin/test_monitor.sh
@@ -4,6 +4,19 @@ set -e
 set -o pipefail
 
 # invoke a test monitor run in a lambda monitor
-
 MONITOR_ERROR=$(curl -w '\n' -X POST 'http://localhost:8080/2015-03-31/functions/function/invocations' -d '{}')
-test "$MONITOR_ERROR = null"
+
+# If the result was null - then we succeeded!
+if [ "${MONITOR_ERROR}" == "null" ]; then
+    exit 0
+fi
+
+# Otherwise - some kind of error occured
+MONITOR_ERROR_TYPE=$(echo "${MONITOR_ERROR}" | jq -r '.errorType')
+if [ "${MONITOR_ERROR_TYPE}" == "errorString" ]; then
+    echo "${MONITOR_ERROR}" | jq -r '.errorMessage' >&2
+else
+    echo "${MONITOR_ERROR}" | jq >&2
+fi
+exit 1
+
