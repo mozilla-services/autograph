b6d1a5ff3c9ad11a014c475599f2c7cbb14e7480
Naomi Kirby <oskirby@gmail.com>
2024-07-06 10:53:10 -0700

    Clean up some formatting

--- bin/run_integration_tests.sh
+++ bin/run_integration_tests.sh
@@ -25,7 +25,8 @@ while test "true" != "$(docker inspect -f {{.State.Running}} autograph-app-hsm)"
 done
 
 # fetch the updated root hash from the app-hsm service
-APP_HSM_NORMANDY_ROOT_HASH=$(docker compose exec app-hsm yq -r '.signers[] | select(.id == "normandy").cacert' /app/autograph.softhsm.yaml | openssl x509 -outform DER | sha256sum | awk '{print $1}')
+APP_HSM_NORMANDY_ROOT_HASH=$(docker compose exec app-hsm yq -r '.signers[] | select(.id == "normandy").cacert' /app/autograph.softhsm.yaml | \
+                             openssl x509 -outform DER | sha256sum | awk '{print $1}')
 
 # start the monitor lambda emulators
 echo "checking autograph monitors"
