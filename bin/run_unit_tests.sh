6e525a6f89512fd60859f1f5ab38ca51f356eccf
Jeff Hodges <jeff@somethingsimilar.com>
2024-08-06 11:39:23 -0700

    move tools/autograph-monitor into top-level make (#943)
    This is review feedback from a previous patch.
    https://github.com/mozilla-services/autograph/pull/941#discussion_r1705636141
    
    This patches moves tools/autograph-monitor is `PACKAGE_NAMES` in the
    top-level Makefile and removes the separated make calls.
    
    Our linting tools (surprisingly!) caught some things in
    contentsignaturepki_test.go in autograph-monitor with this change, so we
    fix this up, as well. (I suspect this "typedNilNotifier" thing isn't
    testing what the authors thought it was, but I'm going for a minimal
    change here. I could be told to rip it out in a follow-up.)
    
    This might mess up the coverage calculations we were previously doing,
    but I'm not sure we've been getting much value out of those. We can fix
    them up later if we do.
    
    This also tries to clean up some unusual ways the autograph-monitor is
    referenced in the rest of the Makefile. I'm not sure what that was
    about. Perhaps, at some point, the original authors had code files in
    that directory with different `package`s in them?

--- bin/run_unit_tests.sh
+++ bin/run_unit_tests.sh
@@ -3,7 +3,6 @@
 set -e
 set -o pipefail
 
-
 # run unit tests inside an app container
 # and report coverage from CI
 #
@@ -16,18 +15,12 @@ fi
 
 if [ "$RACE_TEST" = "1" ]; then
     make race
-    make -C tools/autograph-monitor race
 else
     make test
-    make -C tools/autograph-monitor test
 fi
 
 if [ "$REPORT_COVERAGE" = "true" ]; then
-    cp coverage.out combined-coverage.out
-    # skip 'mode: count' on the first line
-    tail -n +2 tools/autograph-monitor/monitor-coverage.out >> combined-coverage.out
-
     # report coverage
     # ignore failures while coveralls is down for maintenance
-    $(go env GOPATH)/bin/goveralls -coverprofile=combined-coverage.out -service circle-ci || true
+    $(go env GOPATH)/bin/goveralls -coverprofile=coverage.out -service circle-ci || true
 fi
